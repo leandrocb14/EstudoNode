2fe7b2bb662cfd41f8fe5677b0bc5428cc726dc5
leandrocb14 <leandrocb14@gmail.com>
2022-03-01 14:14:01 -0300

    Aprendendo a mexer com process.argv

--- index.js
+++ index.js
@@ -29,26 +29,4 @@ async function pegaArquivo(caminhoDoArquivo) {
 
 }
 
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8'
-//   fs.promises
-//     .readFile(caminhoDoArquivo, encoding)
-//     .then((texto) => {
-//       console.log(chalk.green(texto));
-//     })
-//     .catch((erro) => {
-//       tratarErro(erro);
-//     });
-// }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8'
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//     if (erro) {
-//       tratarErro(erro);
-//     }
-//     console.log(chalk.green(texto));
-//   });
-// }
-
-pegaArquivo('./arquivos/texto1.md')
+export { pegaArquivo }
