750d31c3d4bf6a92a32f2b2bfdf96f4890387819
leandrocb14 <leandrocb14@gmail.com>
2022-03-02 22:39:17 -0300

    Finalizando estudos com jest

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,22 @@
 /*node --experimental-vm-modules node_modules/jest/bin/jest.js */
-import {pegaArquivo} from '../index.js'
+import { pegaArquivo } from '../index.js'
+import { expect, describe, it } from '@jest/globals';
 
-test('Deve ser uma função', () => {
-  expect(typeof pegaArquivo).toBe('function');
-});
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+];
+describe('pegaArquivo::', () => {
+  it('deve ser uma função', () => {
+    expect(typeof pegaArquivo).toBe('function');
+  });
+  it('deve retornar array com resultados', async () => {
+    const resultado = await pegaArquivo('./test/arquivos/texto1.md');
+    expect(resultado).toEqual(arrayResult);
+  });
+  it('deve retornar mensagem que "não há links"', async () => {
+    const resultado = await pegaArquivo('./test/arquivos/texto1_semlinks.md');
+    expect(resultado).toBe('não há links');
+  });
+})
